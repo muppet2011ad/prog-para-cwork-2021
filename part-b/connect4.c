e120096432dc45a42768aa3221d48d98a1d9122a
James Townsend <j.a.townsend2000@gmail.com>
2020-12-15 17:26:35 +0000

    Added comments

--- part-b/connect4.c
+++ part-b/connect4.c
@@ -9,32 +9,36 @@
 char **read_lines (FILE* src, int *num_lines);
 
 struct board_structure {
-    char **grid;
+    char **grid; // Array of strings to store board data
     int width;
-    int height;
+    int height; // Stores bounds of board
 };
 
 board setup_board(){
-    board new_board = calloc (1, sizeof(struct board_structure));
+    board new_board = calloc (1, sizeof(struct board_structure)); // Alloc memory and zero all bytes (makes it easy to distinguish between a board populated with data and one that isn't)
+    if (new_board == NULL) {
+        fprintf(stderr, "Error: failed to allocate memory when setting up board.");
+        exit(1);
+    }
     return new_board;
 }
 
 void cleanup_board(board u){
     if (!(u->grid == NULL)) { // Only try to free the grid data if it's been assigned, otherwise we'll segfault trying to free NULL
         for (int i = 0; i < u->height; i++) {
-            free(u->grid[i]);
+            free(u->grid[i]); // Free each row
         }
-        free(u->grid);
+        free(u->grid); // Free the grid array itself
     }
-    free(u);
+    free(u); // Free the struct
 }
 
 void read_in_file(FILE *infile, board u){
     int rows = 0;
-    char **lines = read_lines(infile, &rows);
-    u->grid = lines;
+    char **lines = read_lines(infile, &rows); // Read in the lines from the file given
+    u->grid = lines; // Copy pointer to lines into board
     u->height = rows;
-    u->width = strlen(lines[0]);
+    u->width = strlen(lines[0]); // Work out the bounds of the board
     for (int i = 0; i < u->height; i++) {
         for (int j = 0; j < u->width; j++) { // Correct any capitals to lowercase and remove any invalid characters
             if (u->grid[i][j] == 'X') { u->grid[i][j] = 'x'; }
@@ -50,14 +54,14 @@ void write_out_file(FILE *outfile, board u){
 
 char next_player(board u){
     int num_x;
-    int num_o;
+    int num_o; // Create counters for each player
     for (int i = 0; i < u->height; i++) {
         for (int j = 0; j < u->width; j++) {
             if (u->grid[i][j] == 'o') { num_o++; }
-            else if (u->grid[i][j] == 'x') { num_x++; }
+            else if (u->grid[i][j] == 'x') { num_x++; } // Increment counters accordingly
         }
     }
-    if (num_x <= num_o) { return 'x'; }
+    if (num_x <= num_o) { return 'x'; } // Make comparison - x goes first so should be returned if equal
     else { return 'o'; }
 }
 
@@ -79,10 +83,10 @@ struct move read_in_move(board u){
 int is_valid_move(struct move m, board u){
     int real_col = m.column - 1;
     int real_row = abs(m.row) - 1; // Convert the move data to indices 
-    if (real_row >= u->height) { return 0; }
-    if (real_col >= u->width || real_col < 0) { return 0; }
-    if (u->grid[0][real_col] != '.') { return 0; }
-    return 1;
+    if (real_row >= u->height) { return 0; } // Check bound on row. No need to check if below zero since -1 indicates no rotation (as m.row was 0)
+    if (real_col >= u->width || real_col < 0) { return 0; } // Check bounds on column. Must be between 0 and the max index, which is width-1
+    if (u->grid[0][real_col] != '.') { return 0; } // Check that there's actually room to place the new token at the top of the grid
+    return 1; // If it passes all these tests, it's a winning move so return 1
 }
 
 char is_winning_move(struct move m, board u){
@@ -103,9 +107,9 @@ char **read_lines (FILE* src, int *num_lines) { // Mostly copied from my part d 
         exit(1);
     }
     int lines_arr_size = 5;
-    char input_buffer[512]; // Create buffer to receive input
+    char input_buffer[514]; // Create buffer to receive input - 514 as it allows 512 chars + \n + \0
     while (!feof(src)) { // Until we reach EOF
-        char *success = fgets(input_buffer, 512, src); // Read from input stream
+        char *success = fgets(input_buffer, 514, src); // Read from input stream
         if (success == NULL) { // Check we haven't just read to EOF
             break; // If we have, stop looping
         }
