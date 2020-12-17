5452570b7793b56a6d1e94412caebcc05e89b358
James Townsend <j.a.townsend2000@gmail.com>
2020-12-17 18:50:09 +0000

    Grid now stored as a bitarray to improve memory efficiency

--- part-b/connect4.c
+++ part-b/connect4.c
@@ -7,6 +7,8 @@
 #include <ctype.h>
 #include"connect4.h"
 
+#define TOKENS_IN_INT (sizeof(int)*4)
+
 typedef struct win_structure {
     int real_row_start;
     unsigned int real_col_start : 9; // Max 512 columns so will never need more than 512 indices
@@ -21,12 +23,14 @@ int get_real_row(board u, int original_row);
 win find_win(board u, char player);
 int real_modulo(int a, int b);
 board copy_board(board u);
-void capitalise_win(board u, win w);
+void capitalise_win(char **display, win w, int width);
 void validate_pointer(void *ptr, int errtype);
 void error(int type);
+char getVal(board u, int row, int col);
+void setVal(board u, int row, int col, char token);
 
 struct board_structure {
-    char **grid; // Array of strings to store board data
+    unsigned int *grid; // Array of strings to store board data
     int width;
     int height; // Stores bounds of board
 };
@@ -40,9 +44,6 @@ board setup_board(){
 void cleanup_board(board u){
     validate_pointer(u, 6);
     if (!(u->grid == NULL)) { // Only try to free the grid data if it's been assigned, otherwise we'll segfault trying to free NULL
-        for (int i = 0; i < u->height; i++) {
-            free(u->grid[i]); // Free each row
-        }
         free(u->grid); // Free the grid array itself
     }
     free(u); // Free the struct
@@ -52,39 +53,41 @@ void read_in_file(FILE *infile, board u){
     validate_pointer(u, 6);
     int rows = 0;
     char **lines = read_lines(infile, &rows); // Read in the lines from the file given
-    u->grid = lines; // Copy pointer to lines into board
     u->height = rows;
     u->width = strlen(lines[0]); // Work out the bounds of the board
+    int members = u->height*u->width;
+    u->grid = calloc((members/TOKENS_IN_INT) + (members%TOKENS_IN_INT != 0), sizeof(int));
     for (int i = 0; i < u->height; i++) {
-        if (strlen(u->grid[i]) != strlen(lines[0])) { error(5); }
+        if (strlen(lines[i]) != strlen(lines[0])) { error(5); }
         for (int j = 0; j < u->width; j++) { // Correct any capitals to lowercase and remove any invalid characters
-            if (u->grid[i][j] == 'X') { u->grid[i][j] = 'x'; }
-            else if (u->grid[i][j] == 'O') { u->grid[i][j] = 'o'; }
-            else if (!(u->grid[i][j] == 'x') && !(u->grid[i][j] == 'o') && !(u->grid[i][j] == '.')) { u->grid[i][j] = '.'; }
+            if (lines[i][j] == 'x' || lines[i][j] == 'X') { setVal(u, i, j, 'x'); }
+            else if (lines[i][j] == 'o' || lines[i][j] == 'O') { setVal(u, i, j, 'o'); }
         }
     }
 }
 
 void write_out_file(FILE *outfile, board u){
-    win test = {23423, 511, 1, 1};
     validate_pointer(outfile, 3);
     validate_pointer(u, 6);
     win x_win = find_win(u, 'x');
     win o_win = find_win(u, 'o');
-    board display;
-    if (x_win.is_win || o_win.is_win) {
-        display = copy_board(u); // We copy the board so we can make display changes (i.e. capitalise winning runs) without ruining the original board
-        if (x_win.is_win) { capitalise_win(display, x_win); }
-        if (o_win.is_win) { capitalise_win(display, o_win); }
+    char **display = malloc(sizeof(char*)*u->height);
+    validate_pointer(display, 7);
+    for (int i = 0; i < u->height; i++) {
+        display[i] = malloc(u->width+1);
+        validate_pointer(display[i], 7);
+        display[i][u->width] = '\0';
+        for (int j = 0; j < u->width; j++) {
+            display[i][j] = getVal(u, i, j);
+        }
     }
-    else {
-        display = u;
-    }
-    for (int i = 0; i < display->height; i++) {
-        fprintf(outfile, "%s\n", display->grid[i]);
+    if (x_win.is_win) { capitalise_win(display, x_win, u->width); }
+    if (o_win.is_win) { capitalise_win(display, o_win, u->width); }
+    for (int i = 0; i < u->height; i++) {
+        fprintf(outfile, "%s\n", display[i]);
     }
     fprintf(outfile, "\n");
-    if (display != u) { cleanup_board(display); }
+    free(display);
 }
 
 char next_player(board u){
@@ -93,8 +96,8 @@ char next_player(board u){
     int num_o = 0; // Create counters for each player
     for (int i = 0; i < u->height; i++) {
         for (int j = 0; j < u->width; j++) {
-            if (u->grid[i][j] == 'o') { num_o++; }
-            else if (u->grid[i][j] == 'x') { num_x++; } // Increment counters accordingly
+            if (getVal(u, i, j) == 'o') { num_o++; }
+            else if (getVal(u, i, j) == 'x') { num_x++; } // Increment counters accordingly
         }
     }
     if (num_x <= num_o) { return 'x'; } // Make comparison - x goes first so should be returned if equal
@@ -128,7 +131,7 @@ int is_valid_move(struct move m, board u){
     int real_row = get_real_row(u, m.row); // Convert the move data to indices 
     if ((real_row < 0 || real_row >= u->height) && m.row != 0) { return 0; } // Check bound on row. No need to check if below zero since -1 indicates no rotation (as m.row was 0)
     if (real_col >= u->width || real_col < 0) { return 0; } // Check bounds on column. Must be between 0 and the max index, which is width-1
-    if (u->grid[0][real_col] != '.') { return 0; } // Check that there's actually room to place the new token at the top of the grid
+    if (getVal(u, 0, real_col) != '.') { return 0; } // Check that there's actually room to place the new token at the top of the grid
     return 1; // If it passes all these tests, it's a winning move so return 1
 }
 
@@ -146,24 +149,24 @@ void play_move(struct move m, board u){
     validate_pointer(u, 6);
     if (!is_valid_move(m, u)) { error(2); }
     char token = next_player(u); // Gets the token that will be played
-    u->grid[0][m.column-1] = token; // Place it in the top row
+    setVal(u, 0, m.column-1, token); // Place it in the top row
     resolve_gravity_single(u, 0, m.column-1); // Apply gravity to let it fall
     if (m.row == 0) { return; } // If m.row was zero then there is no need to rotate
     int real_row = get_real_row(u, m.row); // Otherwise get the array index for the move's row
     if (m.row > 0) { // If rotating to the right
-        char last = u->grid[real_row][u->width-1]; // Rightmost column becomes leftmost so start by copying that
+        char last = getVal(u, real_row, u->width-1); // Rightmost column becomes leftmost so start by copying that
         for (int i = 0; i < u->width; i++) { // Iterate across the row left to right
-            char tmp = u->grid[real_row][i];
-            u->grid[real_row][i] = last;
+            char tmp = getVal(u, real_row, i);
+            setVal(u, real_row, i, last);
             last = tmp; // Swap the item in last with the ith column
             resolve_gravity_above(u, real_row, i); // Apply gravity on the column
         }
     }
     else { // If rotating to the right, the process is the same but right-to-left
-        char last = u->grid[real_row][0];
+        char last = getVal(u, real_row, 0);
         for (int i = u->width-1; i >= 0; i--) {
-            char tmp = u->grid[real_row][i];
-            u->grid[real_row][i] = last;
+            char tmp = getVal(u, real_row, i);
+            setVal(u, real_row, i, last);
             last = tmp;
             resolve_gravity_above(u, real_row, i);
         } 
@@ -177,8 +180,8 @@ win find_win(board u, char player) {
     // Check horizontal wins
     for (int i = 0; i < u->height; i++) {
         for (int j = 0; j < u->width; j++) {
-            char token = u->grid[i][j];
-            if (token == player && u->grid[i][real_modulo(j+1, u->width)] == token && u->grid[i][real_modulo(j+2, u->width)] == token && u->grid[i][real_modulo(j+3, u->width)] == token) {
+            char token = getVal(u, i, j);
+            if (token == player && getVal(u, i, real_modulo(j+1, u->width)) == token && getVal(u, i, real_modulo(j+2, u->width)) == token && getVal(u, i, real_modulo(j+3, u->width)) == token) {
                 return (win) {i, j, 0, 1};
             }
         }
@@ -186,17 +189,17 @@ win find_win(board u, char player) {
     // Check vertical and diagonal wins
     for (int i = 0; i < u->height-3; i++) {
         for (int j = 0; j < u->width; j++) {
-            char token = u->grid[i][j];
+            char token = getVal(u, i, j);
             // Check vertical
-            if (token == player && u->grid[i+1][j] == token && u->grid[i+2][j] == token && u->grid[i+3][j] == token) {
+            if (token == player && getVal(u, i+1, j) == token && getVal(u, i+2, j) == token && getVal(u, i+3, j) == token) {
                 return (win) {i, j, 2, 1};
             }
             // Check diagonal y = -x
-            if (token == player && u->grid[i+1][real_modulo(j+1, u->width)] == token && u->grid[i+2][real_modulo(j+2, u->width)] == token && u->grid[i+3][real_modulo(j+3, u->width)] == token) {
+            if (token == player && getVal(u, i+1, real_modulo(j+1, u->width)) == token && getVal(u, i+2, real_modulo(j+2, u->width)) == token && getVal(u, i+3, real_modulo(j+3, u->width)) == token) {
                 return (win) {i, j, 3, 1};
             }
             // Check diagonal y = x
-            if (token == player && u->grid[i+1][real_modulo(j-1, u->width)] == token && u->grid[i+2][real_modulo(j-2, u->width)] == token && u->grid[i+3][real_modulo(j-3, u->width)] == token) {
+            if (token == player && getVal(u, i+1, real_modulo(j-1, u->width)) == token && getVal(u, i+2, real_modulo(j-2, u->width)) == token && getVal(u, i+3, real_modulo(j-3, u->width)) == token) {
                 return (win) {i, j, 1, 1};
             }
         }
@@ -205,31 +208,30 @@ win find_win(board u, char player) {
     return (win) {0, 0, 0, 0};
 }
 
-void capitalise_win(board u, win w) {
-    validate_pointer(u, 6);
+void capitalise_win(char **display, win w, int width) {
     if (!w.is_win) { return; } // If the win isn't really a win, just don't do anything
-    char token = toupper(u->grid[w.real_row_start][w.real_col_start]);
-    u->grid[w.real_row_start][w.real_col_start] = token;
+    char token = toupper(display[w.real_row_start][w.real_col_start]);
+    display[w.real_row_start][w.real_col_start] = token;
     switch (w.direction) {
         case 0: // Horizontal
-            u->grid[w.real_row_start][real_modulo(w.real_col_start+1, u->width)] = token;
-            u->grid[w.real_row_start][real_modulo(w.real_col_start+2, u->width)] = token;
-            u->grid[w.real_row_start][real_modulo(w.real_col_start+3, u->width)] = token;
+            display[w.real_row_start][real_modulo(w.real_col_start+1, width)] = token;
+            display[w.real_row_start][real_modulo(w.real_col_start+2, width)] = token;
+            display[w.real_row_start][real_modulo(w.real_col_start+3, width)] = token;
             break;
         case 1: // Diagonal y = x
-            u->grid[w.real_row_start+1][real_modulo(w.real_col_start-1, u->width)] = token;
-            u->grid[w.real_row_start+2][real_modulo(w.real_col_start-2, u->width)] = token;
-            u->grid[w.real_row_start+3][real_modulo(w.real_col_start-3, u->width)] = token;
+            display[w.real_row_start+1][real_modulo(w.real_col_start-1, width)] = token;
+            display[w.real_row_start+2][real_modulo(w.real_col_start-2, width)] = token;
+            display[w.real_row_start+3][real_modulo(w.real_col_start-3, width)] = token;
             break;
         case 2: // Vertical
-            u->grid[w.real_row_start+1][w.real_col_start] = token;
-            u->grid[w.real_row_start+2][w.real_col_start] = token;
-            u->grid[w.real_row_start+3][w.real_col_start] = token;
+            display[w.real_row_start+1][w.real_col_start] = token;
+            display[w.real_row_start+2][w.real_col_start] = token;
+            display[w.real_row_start+3][w.real_col_start] = token;
             break;
         case 3: // Diagonal y = -x
-            u->grid[w.real_row_start+1][real_modulo(w.real_col_start+1, u->width)] = token;
-            u->grid[w.real_row_start+2][real_modulo(w.real_col_start+2, u->width)] = token;
-            u->grid[w.real_row_start+3][real_modulo(w.real_col_start+3, u->width)] = token;
+            display[w.real_row_start+1][real_modulo(w.real_col_start+1, width)] = token;
+            display[w.real_row_start+2][real_modulo(w.real_col_start+2, width)] = token;
+            display[w.real_row_start+3][real_modulo(w.real_col_start+3, width)] = token;
             break;
     }
 }
@@ -242,17 +244,17 @@ int get_real_row(board u, int original_row) { // Converts the user-friendly row 
 void resolve_gravity_single(board u, int real_row, int real_col) { // Applies the effects of gravity to a SINGLE token
     validate_pointer(u, 6);
     if (real_row == u->height-1) { return; } // If we're at the bottom row, no problem
-    if (u->grid[real_row+1][real_col] != '.') { return; } // If the space below us is occupied, also no problem
+    if (getVal(u, real_row+1, real_col) != '.') { return; } // If the space below us is occupied, also no problem
     int new_row = real_row;
-    while (!(new_row == u->height-1) && !(u->grid[new_row+1][real_col] != '.')) { new_row++; } // Count through rows until one of the "no problem" conditions is met
-    u->grid[new_row][real_col] = u->grid[real_row][real_col]; // Copy the token into its new position
-    u->grid[real_row][real_col] = '.'; // Leave an empty space where it used to be
+    while (!(new_row == u->height-1) && !(getVal(u, new_row+1, real_col) != '.')) { new_row++; }; // Count through rows until one of the "no problem" conditions is met
+    setVal(u, new_row, real_col, getVal(u, real_row, real_col)); // Copy the token into its new position
+    setVal(u, real_row, real_col, '.'); // Leave an empty space where it used to be
 }
 
 void resolve_gravity_above(board u, int real_row, int real_col) {
     validate_pointer(u, 6);
     for (int i = real_row; i >= 0; i--) { // A token falling can only affect spaces directly above it, so iterate through those
-        if (u->grid[i][real_col] != '.') {
+        if (getVal(u, i, real_col) != '.') {
             resolve_gravity_single(u, i, real_col); // And apply gravity if they contain a token
         }
     }
@@ -299,14 +301,37 @@ board copy_board(board u) {
     board v = setup_board();
     v->width = u->width;
     v->height = u->height;
-    v->grid = malloc(sizeof(char*)*v->height);
+    int members = v->width*v->height;
+    v->grid = malloc(((members/TOKENS_IN_INT) + (members%TOKENS_IN_INT != 0))*sizeof(int));
     validate_pointer(v->grid, 0);
-    for (int i = 0; i < v->height; i++) {
-        v->grid[i] = malloc((sizeof(char)*v->width)+1); // +1 to allow for \0 (forgot this originally so valgrind was complaining)
-        validate_pointer(v->grid[i], 0);
-        strcpy(v->grid[i], u->grid[i]);
-    }
+    memcpy(v->grid, u->grid, ((members/TOKENS_IN_INT) + (members%TOKENS_IN_INT != 0))*sizeof(int));
     return v;
+}
+
+char getVal(board u, int row, int col) {
+    int index = ((row*u->width) + col)/TOKENS_IN_INT; // Calculates the index of the integer we need to fetch in the grid
+    int bit_index = ((row*u->width) + col)%TOKENS_IN_INT; // Calculates the index of the bit pair within that int we need
+    unsigned int stored_bytes = u->grid[index]; // Grabs the int from the grid array
+    //unsigned int stored_bytes = 403179528;
+    stored_bytes = stored_bytes >> (((TOKENS_IN_INT-bit_index)*2)-2); // Bit-shfts it until the bit pair we want occupies the least significant bits
+    int val = 3 & stored_bytes; // Copy these two bits into val
+    if (val == 1) { return 'o'; }
+    else if (val == 2) { return 'x'; } // Return a different char depending on this value
+    else { return '.'; }
+}
+
+void setVal(board u, int row, int col, char token) {
+    int index = ((row*u->width) + col)/TOKENS_IN_INT;
+    int bit_index = ((row*u->width) + col)%TOKENS_IN_INT;
+    unsigned int stored_bytes = u->grid[index]; // Same as getVal up to here
+    unsigned int val = 0; // Default value to write is 0 (this means if we are told to write an invalid token, we just write blank)
+    if (token == 'o') { val = 1; }
+    else if (token == 'x') { val = 2; } // Set val depending on the token given
+    val = val << (((TOKENS_IN_INT-bit_index)*2)-2); // Shift val so our two bits are correctly positioned within the whole int
+    unsigned int clear_flag = ~(3 << (((TOKENS_IN_INT-bit_index)*2)-2)); // Creates a flag to clear the two bits already stored (everything but the two bits we care about is 1)
+    stored_bytes = stored_bytes & clear_flag; // Clear what was already in that bit position
+    stored_bytes = stored_bytes | val; // Copy our val bits into the correct position
+    u->grid[index] = stored_bytes; // Update the array with the new value
 }
 
 void validate_pointer(void *ptr, int errtype) { // Checks if pointer is null, calls error(errtype) if it is
@@ -321,7 +346,8 @@ void error(int type) {
                                 "Could not open file for reading/writing.", // 3
                                 "Invalid input - make sure you input an integer.", // 4
                                 "Board has inconsistent number of columns.", // 5
-                                "Board does not exist." // 6
+                                "Board does not exist.", // 6
+                                "Failed to allocate memory when preparing board for output." // 7
                             };
     fprintf(stderr, "Error: %s\n", messages[type]);
     exit(1);
